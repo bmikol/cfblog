007ea02c3e83199dfb46a409ff01f12aa5fa8a50
Brian Mikol <github@sffogworks.com>
2017-03-30 16:43:25 -0700

    Add Portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'about'     => 'static_pages#about'
   get 'contact'   => 'static_pages#contact'
+  get 'portfolio' => 'static_pages#portfolio'
 
   post 'static_pages/thank_you'
 
